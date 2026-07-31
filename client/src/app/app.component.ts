d69b3a1dc2a3de1794eea8386f15f7112d9e79f4
geshan1234 <geshandhanodya1997@gmail.com>
2026-07-31 19:16:29 +0530

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  private baseUrl: string = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  products: Product[] = [];
+
+  ngOnInit() {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error('Error fetching products:', error),
+      complete: () => console.log('Product fetch complete')
+    });
+  }
 }
